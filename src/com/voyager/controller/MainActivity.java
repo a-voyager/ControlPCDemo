fb7c60057640bf414ddfe29e3e9169282643613d
wuhaojie <w19961009@126.com>
2015-07-14 11:58:13 +0800

    完成搜索服务端.

--- src/com/voyager/controller/MainActivity.java
+++ src/com/voyager/controller/MainActivity.java
@@ -6,6 +6,8 @@ import java.net.Socket;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,13 +15,51 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+/**
+ * @author wuhaojie
+ *
+ */
 public class MainActivity extends Activity implements OnClickListener {
 
+	/**
+	 * 服务端端口
+	 */
 	public static final int PORT = 3000;
+	/**
+	 * handler查找失败
+	 */
+	private static final int FAILED = -1000;
+	/**
+	 * handler查找成功
+	 */
+	private static final int SUCCEED = 1000;
+	/**
+	 * 主界面命令编辑框
+	 */
 	private EditText et_command;
+	/**
+	 * 主界面发送按钮
+	 */
 	private Button btn_send;
+	/**
+	 * WiFi工具类
+	 */
 	private WifiUtils wifiUtils;
+	/**
+	 * 主界面进度条
+	 */
+	private ProgressBar pd_main;
+	/**
+	 * 连接ip超时
+	 */
+	private static final int TIMEOUT = 50;
+	/**
+	 * 最大搜索ip
+	 */
+	private static final int MAX_IP = 256;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +68,9 @@ public class MainActivity extends Activity implements OnClickListener {
 
 		btn_send = (Button) findViewById(R.id.btn_send);
 		et_command = (EditText) findViewById(R.id.et_command);
+		pd_main = (ProgressBar) findViewById(R.id.pd_main);
 		btn_send.setOnClickListener(this);
+		pd_main.setMax(MAX_IP);
 
 		wifiUtils = new WifiUtils(MainActivity.this);
 	}
@@ -38,27 +80,53 @@ public class MainActivity extends Activity implements OnClickListener {
 		new ScanThread().start();
 	}
 
+	/**
+	 * handler处理消息
+	 */
+	private Handler handler = new Handler() {
+		public void handleMessage(android.os.Message msg) {
+			switch (msg.what) {
+			case 1000:
+				Toast.makeText(MainActivity.this, "找到服务端", 0);
+				break;
+
+			case -1000:
+				Toast.makeText(MainActivity.this, "未找到服务端", 0);
+				break;
+			default:
+				pd_main.setProgress(msg.what);
+				break;
+			}
+		};
+	};
+
 	private class ScanThread extends Thread {
+
 		@Override
 		public void run() {
 			String serverIp = wifiUtils.getServerIp();
 			int index = serverIp.lastIndexOf(".") + 1; // 要包含'.', 所以加1
 			serverIp = serverIp.substring(0, index);
 			boolean flag = false;
-			for (int i = 1; i < 12; i++) {
+			for (int i = 1; i < MAX_IP; i++) {
 				Socket socket = new Socket();
 				InetSocketAddress inetSocketAddress = new InetSocketAddress(
 						serverIp + i, PORT);
 				try {
-					socket.connect(inetSocketAddress, 1000);
+					socket.connect(inetSocketAddress, TIMEOUT);
 					socket.close();
+					Message message = new Message();
+					message.what = SUCCEED;
+					message.obj = serverIp + i;
+					handler.sendMessage(message);
 					flag = true;
 					break;
 				} catch (Exception e) {
-					Log.e("ScanThread", "Scan Error:"+serverIp+i);
+					Log.e("ScanThread", "Scan Error:" + serverIp + i);
+					handler.sendEmptyMessage(i);
 				}
-				if (flag) {
-					System.out.println("ip=" + serverIp + i);
+				if (!flag) {
+					handler.sendEmptyMessage(FAILED);
 				}
 			}
 			super.run();
